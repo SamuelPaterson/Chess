2942b8dbdae0a2f6a549f9fdfd82d9c376093330
SamuelPaterson <sam.joe.pat@gmail.com>
2022-04-30 17:01:09 +0100

    Fixes and en passant
    Fixed castling though check. Fixed undo feature. Implemented full move history. Implemented en passant.

--- engine.py
+++ engine.py
@@ -1,9 +1,6 @@
 """
 Storing all information about the current state.
 And determining legal moves.
-
-Can still castle THROUGH check
-Need to add en passent
 """
 
 
@@ -20,17 +17,12 @@ class GameState():
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
         self.whites_turn = True  # If false then its blacks turn
-        self.last_move = ()  # Used for Undo. Will eventually be a move log
-        self.history = []  # Eventually changing this into full move log so can undo multiple moves.
+        self.history = []  # Keeps a list of move history
+        self.moved_pieces = []  # Keeps a list of the piece moved
+        self.captured_pieces = []  # Keeps a list of the piece captured
+
         self.check_mate = False
         self.stale_mate = False
-
-        self.start_row = ()  # All to do with user inputs
-        self.start_col = ()
-        self.end_row = ()
-        self.end_col = ()
-        self.moved_piece = ()
-        self.captured_piece = ()
 
         self.rook00_moved = 0  # Keep track how many times any rook has moved. 00 = top left, 77 = bottom right
         self.rook07_moved = 0
@@ -43,99 +35,116 @@ class GameState():
         self.b_king = (0, 4)
 
     def make_move(self, clicks):  # Handles the movement of all the pieces, and specifies how castling works
-        self.start_row = int(clicks[0][0])
-        self.start_col = int(clicks[0][1])
-        self.end_row = int(clicks[1][0])
-        self.end_col = int(clicks[1][1])
-        self.moved_piece = self.board[self.start_row][self.start_col]
-        self.captured_piece = self.board[self.end_row][self.end_col]
-        self.board[self.start_row][self.start_col] = ".."
-        if self.moved_piece == "wP" and self.end_row == 0:
-            self.board[self.end_row][self.end_col] = "wQ"
-        elif self.moved_piece == "bP" and self.end_row == 7:
-            self.board[self.end_row][self.end_col] = "bQ"
+        start_row = int(clicks[0][0])
+        start_col = int(clicks[0][1])
+        end_row = int(clicks[1][0])
+        end_col = int(clicks[1][1])
+        moved_piece = self.board[start_row][start_col]
+        captured_piece = self.board[end_row][end_col]
+
+        self.board[start_row][start_col] = ".."
+        if moved_piece == "wP" and end_row == 0:  # Pawn Promotion
+            self.board[end_row][end_col] = "wQ"
+        elif moved_piece == "bP" and end_row == 7:
+            self.board[end_row][end_col] = "bQ"
         else:
-            self.board[self.end_row][self.end_col] = self.moved_piece
+            self.board[end_row][end_col] = moved_piece
 
         self.whites_turn = not self.whites_turn
 
-        if self.moved_piece == "wK":  # Updating the kings position and if castling it moves the rook.
-            self.w_king = (self.end_row, self.end_col)
+        if moved_piece == "wK":  # Updating the kings position and if castling it moves the rook.
+            self.w_king = (end_row, end_col)
             self.w_king_moved = self.w_king_moved + 1
-            if self.end_col - self.start_col == 2:
+            if end_col - start_col == 2:  # If the white king castled king side
                 self.board[7][7] = ".."
                 self.board[7][5] = "wR"
                 self.rook77_moved = self.rook77_moved + 1
-            if self.end_col - self.start_col == -2:
+            if end_col - start_col == -2:  # If the white king castled queen side
                 self.board[7][0] = ".."
                 self.board[7][3] = "wR"
                 self.rook70_moved = self.rook70_moved + 1
-        if self.moved_piece == "bK":
-            self.b_king = (self.end_row, self.end_col)
+        if moved_piece == "bK":
+            self.b_king = (end_row, end_col)
             self.b_king_moved = self.b_king_moved + 1
-            if self.end_col - self.start_col == 2:
+            if end_col - start_col == 2:  # If the black king castled king side
                 self.board[0][7] = ".."
                 self.board[0][5] = "bR"
                 self.rook07_moved = self.rook07_moved + 1
-            if self.end_col - self.start_col == -2:
+            if end_col - start_col == -2:  # If the black king castled queen side
                 self.board[0][0] = ".."
                 self.board[0][3] = "bR"
                 self.rook00_moved = self.rook00_moved + 1
 
-        if self.start_row == 0:
-            if self.start_col == 0:
-                self.rook00_moved = self.rook00_moved + 1
-            elif self.start_col == 7:
-                self.rook07_moved = self.rook07_moved + 1
-        if self.start_row == 7:
-            if self.start_col == 0:
-                self.rook70_moved = self.rook70_moved + 1
-            elif self.start_col == 7:
-                self.rook77_moved = self.rook77_moved + 1
+        if moved_piece[1] == "R":
+            if start_row == 0:
+                if start_col == 0:
+                    self.rook00_moved = self.rook00_moved + 1
+                elif start_col == 7:
+                    self.rook07_moved = self.rook07_moved + 1
+            if start_row == 7:
+                if start_col == 0:
+                    self.rook70_moved = self.rook70_moved + 1
+                elif start_col == 7:
+                    self.rook77_moved = self.rook77_moved + 1
 
-        self.last_move = ()
-        self.history.append(clicks)  # Append with moves
+        if moved_piece[1] == "P" and abs(start_col - end_col) == 1 and captured_piece == "..":  # If en passant
+            if moved_piece[0] == "w":
+                self.board[end_row + 1][end_col] = ".."
+            if moved_piece[0] == "b":
+                self.board[end_row - 1][end_col] = ".."
+
+        self.history.append(clicks)
+        self.moved_pieces.append(moved_piece)
+        self.captured_pieces.append(captured_piece)
 
     def undo(self):
-        if len(self.history) != 0:
-            self.last_move = self.history.pop()
-            self.board[self.start_row][self.start_col] = self.moved_piece
-            self.board[self.end_row][self.end_col] = self.captured_piece
+        if len(self.history) != 0:  # If there is a move to undo
+            last_move = self.history.pop()
+            moved_piece = self.moved_pieces.pop()
+            captured_piece = self.captured_pieces.pop()
+            self.board[last_move[0][0]][last_move[0][1]] = moved_piece
+            self.board[last_move[1][0]][last_move[1][1]] = captured_piece  # Move pieces back
             self.whites_turn = not self.whites_turn
 
-            if self.moved_piece == "wK":  # Updating kings position and undoing castling.
-                self.w_king = (self.start_row, self.start_col)
+            if moved_piece == "wK":  # Updating kings position and undoing castling.
+                self.w_king = (last_move[0])
                 self.w_king_moved = self.w_king_moved - 1
-                if self.end_col - self.start_col == 2:
+                if last_move[1][1] - last_move[0][1] == 2:  # If the white king castled king side.
                     self.board[7][7] = "wR"
                     self.board[7][5] = ".."
                     self.rook77_moved = self.rook77_moved - 1
-                if self.end_col - self.start_col == -2:
+                elif last_move[1][1] - last_move[0][1] == -2:  # If the white king castled queen side.
                     self.board[7][0] = "wR"
                     self.board[7][3] = ".."
                     self.rook70_moved = self.rook70_moved - 1
-            if self.moved_piece == "bK":
-                self.b_king = (self.start_row, self.start_col)
+            if moved_piece == "bK":
+                self.b_king = (last_move[0])
                 self.b_king_moved = self.b_king_moved - 1
-                if self.end_col - self.start_col == 2:
+                if last_move[1][1] - last_move[0][1] == 2:  # If the black king castled king side
                     self.board[0][7] = "bR"
                     self.board[0][5] = ".."
                     self.rook07_moved = self.rook07_moved - 1
-                if self.end_col - self.start_col == -2:
+                if last_move[1][1] - last_move[0][1] == -2:
                     self.board[0][0] = "bR"
                     self.board[0][3] = ".."
                     self.rook00_moved = self.rook00_moved - 1
 
-            if self.start_row == 0:
-                if self.start_col == 0:
+            if last_move[0][0] == 0:
+                if last_move[0][1] == 0:
                     self.rook00_moved = self.rook00_moved - 1
-                elif self.start_col == 7:
+                elif last_move[0][1] == 7:
                     self.rook07_moved = self.rook07_moved - 1
-            if self.start_row == 7:
-                if self.start_col == 0:
+            if last_move[0][0] == 7:
+                if last_move[0][1] == 0:
                     self.rook70_moved = self.rook70_moved - 1
-                elif self.start_col == 7:
+                elif last_move[0][1] == 7:
                     self.rook77_moved = self.rook77_moved - 1
+
+            if moved_piece[1] == "P" and abs(last_move[0][1] - last_move[1][1]) == 1 and captured_piece == "..":
+                if moved_piece[0] == "w":
+                    self.board[last_move[1][0] + 1][last_move[1][1]]= "bP"
+                if moved_piece[0] == "b":
+                    self.board[last_move[1][0] - 1][last_move[1][1]] = "wP"
 
     '''For every possible moves (apart from castling) check if it is valid by: Making the move, then generate all 
     possible responses, then seeing if those responses take the king. '''
@@ -323,6 +332,7 @@ class GameState():
 
     def get_pawn_moves(self, y, x, moves):
         if self.board[y][x][0] == "w":
+            self.get_en_passant(y, x, moves, "w")
             if 0 <= y - 1 <= 7:
                 if self.board[y - 1][x] == "..":
                     moves.append([(y, x), (y - 1, x)])
@@ -337,6 +347,7 @@ class GameState():
                         moves.append([(y, x), (y - 1, x + 1)])
 
         if self.board[y][x][0] == "b":
+            self.get_en_passant(y, x, moves, "b")
             if 0 <= y + 1 <= 7:
                 if self.board[y + 1][x] == "..":
                     moves.append([(y, x), (y + 1, x)])
@@ -350,6 +361,25 @@ class GameState():
                     if self.board[y + 1][x + 1][0] == "w":
                         moves.append([(y, x), (y + 1, x + 1)])
 
+    def get_en_passant(self, y, x, moves, colour):
+        if len(self.history) != 0:
+            last_move = self.history[-1]
+            distance_moved = abs(last_move[0][0] - last_move[1][0])
+            last_move_col = last_move[0][1]  # Because only looking at pawns start or end column are the same.
+            last_piece_moved = self.moved_pieces[-1]
+            if colour == "w" and last_piece_moved == "bP" and y == 3:  # Only row that white can play en passant
+                if distance_moved == 2:  # The pawn moved two spaces
+                    if last_move_col == (x + 1):  # If black pawn is in next col.
+                        moves.append([(y, x), (y - 1, x + 1)])
+                    elif last_move_col == (x - 1):
+                        moves.append([(y, x), (y - 1, x - 1)])
+            if colour == "b" and last_piece_moved == "wP" and y == 4:  # Only row that black can play en passant
+                if distance_moved == 2:  # The pawn moved two spaces
+                    if last_move_col == (x + 1):  # If white pawn is in next col.
+                        moves.append([(y, x), (y + 1, x + 1)])
+                    elif last_move_col == (x - 1):
+                        moves.append([(y, x), (y + 1, x - 1)])
+
     def get_castle_moves(self, moves):  # Check if King is in check and then if the king would move through check
         opp_moves = self.spaces_attacked()
         castle00 = True
@@ -360,22 +390,24 @@ class GameState():
         for move in opp_moves:
             if self.whites_turn and (move[1] == self.w_king):
                 not_in_check = False
-            elif self.whites_turn == False and (move[1] == self.b_king):
+                break
+            elif not self.whites_turn and (move[1] == self.b_king):
                 not_in_check = False
-            else:
-                if move[1] == (0, 4):
+                break
+            else:  # If not in check spaces inbetween king and rook are not under attack
+                if move[1] == (0, 3):
                     castle00 = False
-                if move[1] == (0, 6):
+                if move[1] == (0, 5):
                     castle07 = False
-                if move[1] == (7, 4):
+                if move[1] == (7, 3):
                     castle70 = False
-                if move[1] == (7, 6):
+                if move[1] == (7, 5):
                     castle77 = False
         if castle00 and not_in_check and (self.rook00_moved == 0) and (self.b_king_moved == 0):
             self.get_00castle(moves)
         if castle07 and not_in_check and (self.rook07_moved == 0) and (self.b_king_moved == 0):
             self.get_07castle(moves)
-        if castle70 and not_in_check and (self.rook70_moved == 0) and (self.w_king_moved ==0 ):
+        if castle70 and not_in_check and (self.rook70_moved == 0) and (self.w_king_moved ==0):
             self.get_70castle(moves)
         if castle77 and not_in_check and (self.rook77_moved == 0) and (self.w_king_moved == 0):
             self.get_77castle(moves)
@@ -398,10 +430,9 @@ class GameState():
         if (self.board[self.w_king[0]][self.w_king[1] + 1] == "..") and (self.board[self.w_king[0]][self.w_king[1] + 2] == ".."):
             moves.append([(self.w_king[0], self.w_king[1]), (self.w_king[0], self.w_king[1] + 2)])
 
-
     def all_check_checker(self):
         moves = self.get_all_moves()  # Gets all possible moves
-        self.get_castle_moves(moves)  # Gets all castling moves
+        self.get_castle_moves(moves)  # Gets all castling moves and append to moves
         for i in range(len(moves) - 1, -1, -1):
             self.make_move(moves[i])  # Makes that move
             self.whites_turn = not self.whites_turn
