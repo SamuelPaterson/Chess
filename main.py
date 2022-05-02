9bb141374c50f244bdd51e13b2d0c98ca0c3f7c3
SamuelPaterson <sam.joe.pat@gmail.com>
2022-05-02 13:42:06 +0100

    Added Start Screen
    Added a start screen where you can choose starting colour. Currently does nothing.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from engine import *
 # Define board and spaces sizes
 width = height = 512
 square_size = width / 8
+dark_squares = [47, 130, 255]  # RGB Colour for the dark squares.
 
 '''
 Initialize dictionary of images
@@ -21,25 +22,40 @@ def load_images():
     images["cross"] = pg.transform.scale(pg.image.load("images/cross.png"), (square_size, square_size))
     images["cross"].set_alpha(170)
 
+
 '''
 Class for all my buttons
 '''
+
+
 class Button:
     def __init__(self, screen, x, y, text, button_width, button_height):
         self.screen = screen
-        self.box = pg.Rect(x, y, button_width, button_height)
-        self.font = pg.font.SysFont("arial.ttf", 21, False, False)
-        self.text_display = self.font.render(text, False, pg.Color('Grey'))
-        self.text_location = pg.Rect(0, 0, button_width, button_height).move(button_width / 2 - self.text_display.get_width() / 2, button_height / 2 - self.text_display.get_height() / 2)
+        self.box = pg.Rect(x + 5, y + 5, button_width - 10, button_height - 10)
+        self.outside_box = pg.Rect(x, y, button_width, button_height)
+        self.font = pg.font.SysFont("arial.ttf", 30, False, False)
+        self.text_display = self.font.render(text, False, pg.Color('Black'))
+        self.text_location = pg.Rect(x, y, button_width, button_height).move(
+            button_width / 2 - self.text_display.get_width() / 2,
+            button_height / 2 - self.text_display.get_height() / 2)
 
     def draw(self):
-        pg.draw.rect(self.screen, pg.Color("Black"), self.box)
+        mouse_pos = pg.mouse.get_pos()
+        if self.outside_box.collidepoint(mouse_pos):
+            if pg.mouse.get_pressed()[0] == 1:
+                return True
+
+        # Draw button on screen
+        pg.draw.rect(self.screen, pg.Color("Black"), self.outside_box)
+        pg.draw.rect(self.screen, pg.Color("Grey"), self.box)
         self.screen.blit(self.text_display, self.text_location)
 
 
 '''
 User input and updating graphics
 '''
+
+
 def main():
     pg.init()
     screen = pg.display.set_mode((width, height))
@@ -53,9 +69,6 @@ def main():
     game_over = False
     start_screen = True
 
-    white_button = Button(screen, 0, 0, "White", square_size, square_size)
-    reset_button = Button(screen, 0, 0, "Reset", square_size, square_size)
-
     clicked_square = ()  # Last user click
     clicks = []  # Tracks the past 2 clicks
 
@@ -66,7 +79,11 @@ def main():
                 run = False
 
             if start_screen:
-                start_screen = False
+                player_colour = game_start(screen, gs, piece_moves)
+                if player_colour == "w":
+                    start_screen = False
+                elif player_colour == "b":
+                    start_screen = False
             else:
                 if not game_over:
                     if e.type == pg.MOUSEBUTTONDOWN:  # Mouse Inputs
@@ -97,13 +114,17 @@ def main():
                             gs.undo()
                             legal_moves = gs.all_check_checker()
                     draw_game(screen, gs, piece_moves)
-                elif game_over:
-                    draw_game(screen, gs, piece_moves)
-                    reset_button.draw()
 
         if gs.check_mate or gs.stale_mate:
             game_over = True
-            game_ended(gs.check_mate, gs.whites_turn, screen)
+            if game_ended(gs.check_mate, gs.whites_turn, screen):  # If reset button is pressed
+                gs = GameState()
+                legal_moves = gs.all_check_checker()
+                piece_moves = []
+                game_over = False
+                start_screen = True
+                clicked_square = ()
+                clicks = []
 
         clock.tick(60)
         pg.display.flip()
@@ -115,7 +136,6 @@ All graphics (might want to split up)
 
 
 def draw_game(screen, gs, piece_moves):
-    dark_squares = [47, 130, 255]  # RGB Colour for the dark squares.
     for x in range(8):
         for y in range(8):
             if x % 2 == 0:  # Logic to colour the squares.
@@ -141,8 +161,27 @@ def draw_game(screen, gs, piece_moves):
         screen.blit(images["cross"], (y * square_size, x * square_size))
 
 
-def game_ended(check_mate, whites_turn, screen):  # Draws post game scenarios
+def game_start(screen, gs, piece_moves):
+    player_colour = "None"
+    white_button = Button(screen, 1 * square_size, 5 * square_size, "White", square_size * 2, square_size)
+    black_button = Button(screen, 5 * square_size, 5 * square_size, "Black", square_size * 2, square_size)
+    draw_game(screen, gs, piece_moves)
+
     font = pg.font.SysFont("arial.ttf", 55, False, False)
+    text_display = font.render("Pick a Colour", False, pg.Color('Grey'))
+    text_location = pg.Rect(0, 0, width, height).move(width / 2 - text_display.get_width() / 2,
+                                                      0.375 * height - text_display.get_height() / 2)
+    pg.draw.rect(screen, pg.Color("Black"), pg.Rect(0, 0.25 * height, width, 0.25 * height))
+    screen.blit(text_display, text_location)
+
+    if white_button.draw():
+        player_colour = "w"
+    if black_button.draw():
+        player_colour = "b"
+    return player_colour
+
+
+def game_ended(check_mate, whites_turn, screen):  # Draws post game scenarios
     if check_mate:
         if whites_turn:
             text = "Checkmate! Black Wins!"
@@ -150,10 +189,17 @@ def game_ended(check_mate, whites_turn, screen):  # Draws post game scenarios
             text = "Checkmate! White Wins!"
     else:
         text = "Stalemate"
+    font = pg.font.SysFont("arial.ttf", 55, False, False)
     text_display = font.render(text, False, pg.Color('Grey'))
-    text_location = pg.Rect(0, 0, width, height).move(width/2 - text_display.get_width()/2, height/2 - text_display.get_height()/2)
-    pg.draw.rect(screen, pg.Color("Black"), pg.Rect(0, 0.375*height, width, 0.25*height))
+    text_location = pg.Rect(0, 0, width, height).move(width / 2 - text_display.get_width() / 2,
+                                                      0.375 * height - text_display.get_height() / 2)
+    pg.draw.rect(screen, pg.Color("Black"), pg.Rect(0, 0.25 * height, width, 0.25 * height))
     screen.blit(text_display, text_location)
+
+    reset_button = Button(screen, 3 * square_size, 5 * square_size, "Reset", square_size * 2, square_size)
+    if reset_button.draw():
+        return True
+
 
 if __name__ == "__main__":
     main()
